2a385ac99aea8faa965ae08b6a19d6de2ce50e6e
LeonWellmans <496228@student.fontys.nl>
2025-04-09 12:04:57 +0200

    Add documentation

--- src/main/java/com/example/deesee/restapi/SuperheroController.java
+++ src/main/java/com/example/deesee/restapi/SuperheroController.java
@@ -28,6 +28,12 @@ public class SuperheroController {
         this.encryptionKey = encryptionKey;
     }
 
+    /**
+     *
+     * @param superpowers: Optional list of superpowers
+     * @param encrypted: Optional boolean to return encrypted superheroes
+     * @return: Response with a list of superhero as maps
+     */
     @GetMapping
     public ResponseEntity<List<Map<String, Object>>> getSuperheroes(
             @RequestParam(required = false) List<String> superpowers,
@@ -48,7 +54,11 @@ public class SuperheroController {
         return ResponseEntity.ok(mapSuperheroesToResponse(superheroes));
     }
 
-
+    /**
+     *
+     * @param superheroDTO: Data transfer object containing superhero information
+     * @return: Response with the created superhero as a map
+     */
     @PostMapping
     public ResponseEntity<Map<String, Object>> createSuperhero(@RequestBody SuperheroDTO superheroDTO) {
         Superhero superhero = superheroDTO.toSuperhero();
@@ -56,6 +66,11 @@ public class SuperheroController {
         return ResponseEntity.status(HttpStatus.CREATED).body(mapSuperheroToResponse(superhero));
     }
 
+    /**
+     *
+     * @param superheroesDTO: List of superhero information
+     * @return: Response with a list of created superheroes as maps
+     */
     @PostMapping("/batch")
     public ResponseEntity<List<Map<String, Object>>> createSuperheroes(@RequestBody List<SuperheroDTO> superheroesDTO) {
         List<Superhero> superheroes = superheroesDTO.stream()
@@ -65,12 +80,22 @@ public class SuperheroController {
         return ResponseEntity.status(HttpStatus.CREATED).body(mapSuperheroesToResponse(superheroes));
     }
 
+    /**
+     *
+     * @param superheroes: List of superhero entities
+     * @return: List of maps representing each superhero's details
+     */
     private List<Map<String, Object>> mapSuperheroesToResponse(List<Superhero> superheroes) {
         return superheroes.stream()
                 .map(this::mapSuperheroToResponse)
                 .collect(Collectors.toList());
     }
 
+    /**
+     *
+     * @param superhero: A single superhero entity
+     * @return: A map containing superhero's details
+     */
     private Map<String, Object> mapSuperheroToResponse(Superhero superhero) {
         Map<String, Object> response = new HashMap<>();
         response.put("name", superhero.getName());
@@ -80,6 +105,11 @@ public class SuperheroController {
         return response;
     }
 
+    /**
+     *
+     * @param stringSuperpowers: List o superpowers as strings
+     * @return: List of valid superpower as enums
+     */
     private List<Superpower> stringToEnum(List<String> stringSuperpowers){
         return stringSuperpowers.stream()
                 .map(String::toUpperCase)
